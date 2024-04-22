4e30426b03fd43d8408442737fff827cd55467ec
shaikasfiya1 <shaik.asfiya@accenture.com>
2024-04-23 00:56:55 +0530

    commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //this is to populate the lead date in salesforce because it will not populate directly
-let captchachecked=false;
+let captchachecked=true;
 function beforesubmit(event)
 {   
     if(captchachecked)
