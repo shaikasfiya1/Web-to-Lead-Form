8ebef28cdbdd27b6761cb16e0d6e2dbeb934ed48
shaikasfiya1 <shaik.asfiya@accenture.com>
2024-04-23 00:33:02 +0530

    done changes

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 //this is to populate the lead date in salesforce because it will not populate directly
-function beforesubmit()
-{
-    let outputdate=document.querySelector('.outputdate');
-    let inputdate=document.querySelector('.inputdate');
-    console.log('inputdate'+ inputdate.value+'outputdate'+outputdate.value );
-    let datevar=new Date(inputdate.value).toLocaleDateString("en-IN");
-    outputdate.value=datevar;
+let captchachecked=false;
+function beforesubmit(event)
+{   
+    if(captchachecked)
+    {
+        let outputdate=document.querySelector('.outputdate');
+        let inputdate=document.querySelector('.inputdate');
+        console.log('inputdate'+ inputdate.value+'outputdate'+outputdate.value );
+        let datevar=new Date(inputdate.value).toLocaleDateString("en-IN");
+        outputdate.value=datevar;
+       
+    }
+    else{
+        alert("Please check the recaptcha!");
+        event.preventDefault();
+    }
+    
 }
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+function capthasuccess()
+{
+    captchachecked=true;
+
+}
